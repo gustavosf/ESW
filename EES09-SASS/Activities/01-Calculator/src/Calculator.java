8b257072532fa33fe1b857cc949eda729d5fc4f7
Gustavo Seganfredo <gustavosf@gmail.com>
2015-06-28 16:26:15 -0300

    Created a simple calculator web service

---
+++ EES09-SASS/Activities/01-Calculator/src/Calculator.java
@@ -0,0 +1,39 @@
+/**
+ * Simple calculator
+ * @author Gustavo Seganfredo
+ */
+public class Calculator {
+
+	/**
+	 * Sums two numbers
+	 * @return a+b
+	 */
+	public float sum(float a, float b) {
+		return a + b;
+	}
+	
+	/**
+	 * Subtracts two numbers
+	 * @return a-b
+	 */
+	public float sub(float a, float b) {
+		return a - b;
+	}
+	
+	/**
+	 * Multiply two numbers
+	 * @return a*b
+	 */
+	public float mult(float a, float b) {
+		return a * b;
+	}
+	
+	/**
+	 * Divides two numbers
+	 * @return a/b
+	 */
+	public float div(float a, float b) {
+		return a / b;
+	}
+	
+}
