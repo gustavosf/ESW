2941af10023dbc8313bd854b0b781ae8fcda3ead
Gustavo Seganfredo <gustavosf@gmail.com>
2015-06-28 16:42:23 -0300

    Included client to consume new timeserver method

---
+++ EES09-SASS/Activities/01-TimeServerExtended/ts/TimeClient.java
@@ -0,0 +1,18 @@
+package ts;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
+
+class TimeClient {
+	public static void main(String args[]) throws Exception {
+		URL url = new URL("http://127.0.0.1:9876/ts?wsdl");
+		QName qname = new QName("http://ts/",  "TimeServerImplService");
+		Service ws = Service.create(url, qname);
+		TimeServer ts = ws.getPort(TimeServer.class);
+
+		System.out.println(ts.getTimeForGMT(0));
+		System.out.println(ts.getTimeForGMT(3));
+		System.out.println(ts.getTimeForGMT(-3));
+		System.out.println(ts.getTimeForGMT(-12));
+	}
+}
